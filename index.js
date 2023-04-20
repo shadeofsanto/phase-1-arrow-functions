deb91fe6158e957c4c34f5f55f83430f0ddbb4f0
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-04-20 09:17:37 -0500

    Lab Completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (divOne, divTwo) => divOne / divTwo;
+divide(2000, 100)
+
+const square = (squareNumOne) => squareNumOne * squareNumOne
+square(2)
+
+const add = (numOne, numTwo) => numOne + numTwo
+add(3,4)
